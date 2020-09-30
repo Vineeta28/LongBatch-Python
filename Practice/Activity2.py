b25bfebd54babec2a0198979e3f4ae2e4df2e9c6
Vineeta28 <57710204+Vineeta28@users.noreply.github.com>
2020-09-30 16:26:42 +0530

    Commit

---
+++ Practice/Activity2.py
@@ -0,0 +1,5 @@
+num = int (input( "Type the number: " ))
+if (num % 2 == 0):
+    print("Number is even number")
+else:
+    print("Number is odd number")
---
+++ Practice/Activity7.py
@@ -0,0 +1,7 @@
+numb = list(input("Enter a sequence of comma separated values: ").split(", "))
+sum = 0
+
+for i in num:
+  sum += int(num)
+
+print(sum)
